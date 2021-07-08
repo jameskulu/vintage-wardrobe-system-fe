5c79544be68c4763d07f6cc64066dd6ba6704840
AnupShahi11 <shahirocks11@gmail.com>
2021-07-08 19:16:50 +0545

    Login Functionalities added

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
+
+    const history = useHistory()
+
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    const userLogin = async(e) => {
+        e.preventDefault()
+
+        try {
+            const loginResponse = await axios.post('http://localhost:5000/api/users/login', {email, password})
+
+            localStorage.setItem('auth-token',loginResponse.data.token)
+            toast.success('Login Successful.')
+            history.push('/')
+
+        }
+        catch(err){
+            toast.error(`${err.response.data.message}`)
+        }
+
+    }
     return (
         <div className="container">
             <form action="">
@@ -15,19 +39,23 @@ const Login = () => {
                             name="email"
                             id="email"
                             autoComplete="off"
+                            value={email} 
+                            onChange={e => setEmail(e.target.value)}
                         />
                         <input
                             type="password"
                             placeholder="Password"
-                            name="pwd"
+                            name="password"
                             id="pwd"
                             autoComplete="off"
+                            value={password} 
+                            onChange={e => setPassword(e.target.value)}
                         />
                         <Link id="login-link" to="#">
                             Forgot Password?
                         </Link>
 
-                        <button type="button" className="loginbtn">
+                        <button type="button" className="loginbtn" onClick ={userLogin}>
                             LOGIN
                         </button>
 
