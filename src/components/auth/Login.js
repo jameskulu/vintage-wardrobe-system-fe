0102e4a1da471c38e13561b3354efcb979c407b5
James Kulu <jameskulu55@gmail.com>
2021-07-08 16:58:40 +0545

    test

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return (
+    return ( 
         <div className="container">
             <form action="">
                 <div className="login-form-container">
