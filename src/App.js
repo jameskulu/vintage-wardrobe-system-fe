5a9383bb64a65ce25541884117ff0f1c6f571409
James Kulu <jameskulu55@gmail.com>
2021-07-09 22:16:45 +0545

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
+import axios from 'axios';
 
+import UserContext from './context/UserContext';
+
+// Components
 import Home from './components/pages/Home/Home';
 import Header from './components/layout/Header/Header';
 import Login from './components/auth/Login';
@@ -9,33 +13,70 @@ import Signup from './components/auth/Signup';
 import VerifyEmail from './components/auth/VerifyEmail';
 import ForgotPassword from './components/auth/ForgotPassword';
 import ResetPassword from './components/auth/ResetPassword';
+import { useEffect, useState } from 'react';
 
 toast.configure();
 function App() {
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined,
+    });
+
+    useEffect(() => {
+        const checkLoggedIn = async () => {
+            let token = localStorage.getItem('auth-token');
+            if (token === null) {
+                localStorage.setItem('auth-token', '');
+                token = '';
+            }
+
+            const tokenResponse = await axios.post(
+                `${process.env.REACT_APP_API_URL}/api/users/tokenIsValid`,
+                null,
+                { headers: { Authorization: 'Bearer ' + token } }
+            );
+
+            if (tokenResponse.data) {
+                const userResponse = await axios.get(
+                    `${process.env.REACT_APP_API_URL}/api/users`,
+                    { headers: { Authorization: 'Bearer ' + token } }
+                );
+
+                setUserData({
+                    token,
+                    user: userResponse.data,
+                });
+            }
+        };
+        checkLoggedIn();
+    }, []);
+
     return (
         <BrowserRouter>
-            <div className="App">
-                <Header />
+            <UserContext.Provider value={{ userData, setUserData }}>
+                <div className="App">
+                    <Header />
 
-                <Route exact path="/" component={Home} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/Signup" component={Signup} />
-                <Route
-                    exact
-                    path="/forgot-password"
-                    component={ForgotPassword}
-                />
-                <Route
-                    exact
-                    path="/reset-password/:token"
-                    component={ResetPassword}
-                />
-                <Route
-                    exact
-                    path="/verify-email/:token"
-                    component={VerifyEmail}
-                />
-            </div>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/Signup" component={Signup} />
+                    <Route
+                        exact
+                        path="/forgot-password"
+                        component={ForgotPassword}
+                    />
+                    <Route
+                        exact
+                        path="/reset-password/:token"
+                        component={ResetPassword}
+                    />
+                    <Route
+                        exact
+                        path="/verify-email/:token"
+                        component={VerifyEmail}
+                    />
+                </div>
+            </UserContext.Provider>
         </BrowserRouter>
     );
 }
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import './header.css';
 import { Link } from 'react-router-dom';
+// import AuthOptions from '../../auth/authOptions'
+import UserContext from '../../../context/UserContext';
+import Logo from '../../../images/logo.png'
 
 const Header = () => {
+    const { userData } = useContext(UserContext);
+
+    const toggleMenu = () => {
+        document.getElementById('navbar').classList.toggle('toggle');
+    };
+
     return (
-        <div>
-            <Link className="btn btn-primary" to="/login" role="button">
-                Login
-            </Link>
-            <Link className="btn btn-primary" to="/signup" role="button">
-                SignUp
-            </Link>
-        </div>
+        <header>
+            <div className="inner-header">
+                <Link to="/">
+                    <div className="logo">
+                        <img src={Logo} alt="" />
+                    </div>
+                </Link>
+
+                <nav>
+                    <i
+                        onClick={toggleMenu}
+                        id="toggle-button"
+                        className="fas fa-bars"
+                    ></i>
+                    <ul id="navbar">
+                        <li>
+                            <Link className="active" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/category">Category</Link>
+                        </li>
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+
+                        {userData.user ? (
+                            userData.user.role === 'admin' ? (
+                                <li>
+                                    <Link to="/admin">Admin</Link>
+                                </li>
+                            ) : null
+                        ) : null}
+
+                        {/* <AuthOptions /> */}
+
+                        <li>
+                            <Link to="/s">
+                                <i className="fas fa-search"></i>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/login">
+                                <button>Login</button>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
 };
 
