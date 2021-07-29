e13ccd97f3a9757ebf685eae2b22e8e1dfb95cfc
AnupShahi11 <shahirocks11@gmail.com>
2021-07-29 17:21:35 +0545

    order functionalities added

--- src/components/pages/Customer/Order/Order.js
+++ src/components/pages/Customer/Order/Order.js
@@ -1,7 +1,35 @@
 import '../Cart/cart.css'
 import dress1 from '../../../../images/dress1.jpg'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Order = () => {
+
+    const [myOrders, setMyOrders] = useState([]);
+    const [isDisabled, setDisabled] = useState(false);
+
+
+    useEffect(()=> {
+        const getMyOrder = async () => {
+            const token = localStorage.getItem('auth-token');
+            const myOrderResponse = await axios.get(
+                `${process.env.REACT_APP_API_URL}/api/users/orders`,
+                { headers: { Authorization: 'Bearer ' + token } }
+            );
+            const sortedMyOrderResponse = myOrderResponse.data.data.reverse();
+            if(sortedMyOrderResponse.data.data.status == "pending"){
+              setDisabled(true);
+            }
+            
+            setMyOrders(sortedMyOrderResponse);
+
+        };
+        getMyOrder();
+
+        
+
+    }, []);
+
     return (
 
         <div class="container-fluid cart">
@@ -21,6 +49,7 @@ const Order = () => {
       
           <table class="table ">
         <thead class="thead-light">
+
           <tr>
             <th scope="col"></th>
             <th scope="col">Item</th>
@@ -31,8 +60,11 @@ const Order = () => {
           </tr>
         </thead>
         <tbody>
+        {myOrders.map((order)=>(
+          
+          
           <tr>
-            
+      
             <td><img
                    src={dress1}
                    class="img-fluid img-thumbnail"
@@ -40,59 +72,28 @@ const Order = () => {
                  />
             </td>
             <td> 
-              <p class="text-uppercase">Hutch</p>
-                  <p>Printed Leona Dress</p>
-                  <p>$355</p>
+              <p class="text-uppercase">{order.item.name}</p>
+                  <p>{order.item.description}</p>
+                  <p>{order.item.price}</p>
             </td>
             <td>
-              <p>Sat, Jun 19 - Tue, Jun 22 (4 days)</p>
+              <p> {order.startDate} - {order.endDate}</p>
             </td>
             <td>
-              <p class="font-weight-bold">$64.00</p>
+              <p class="font-weight-bold">{order.totalPrice}</p>
             </td>
             <td >
-              <p>Approved</p>
+              <p>{order.status}</p>
               
             </td>
             <td>
-              <button type="button" class="btn btn-secondary" disabled>Cancel</button>
+              <button type="button" class="btn btn-secondary" disabled ={isDisabled}>Cancel</button>
             </td>
           </tr>
          
-         
+          ))}
         </tbody>
       
-        <tbody>
-          <tr>
-            
-            <td><img
-                   src={dress1}
-                   class="img-fluid img-thumbnail"
-                   alt="Dress"
-                 />
-            </td>
-            <td> 
-              <p class="text-uppercase">Hutch</p>
-                  <p>Printed Leona Dress</p>
-                  <p>$355</p>
-            </td>
-            <td>
-              <p>Sat, Jun 19 - Tue, Jun 22 (4 days)</p>
-            </td>
-            <td>
-              <p class="font-weight-bold">$64.00</p>
-            </td>
-            <td >
-              <p>Pending</p>
-              
-            </td>
-            <td>
-              <button type="button" class="btn btn-danger">Cancel</button>
-            </td>
-          </tr>
-         
-         
-        </tbody>
       </table>
       
       
