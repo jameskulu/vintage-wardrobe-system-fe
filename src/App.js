d24c796c82b1f11306a2278a3540f84926a1aa52
AnupShahi11 <shahirocks11@gmail.com>
2021-08-04 13:20:22 +0545

    wishlist design Added

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import Order from './components/pages/Customer/Order/Order';
 import Checkout from './components/pages/Customer/Checkout/Checkout';
 import CheckoutComplete from './components/pages/Customer/Checkout/CheckoutComplete';
 import About from './components/pages/AboutUs/About';
+import Wishlist from './components/pages/Customer/Wishlist/Wishlist';
 
 toast.configure();
 function App() {
@@ -115,6 +116,7 @@ function App() {
 
                     <Route exact path="/renter/order" component={RentOrder} />
                     <Route exact path="/aboutus" component={About} />
+                    <Route exact path="/wishlist" component={Wishlist} />
 
                     <Route exact path="/s" component={SearchPage} />
                     <Footer />
