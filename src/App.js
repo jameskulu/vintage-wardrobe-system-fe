8deae551c12bcab3b17098119917e33d8af2c5f3
James Kulu <jameskulu55@gmail.com>
2021-08-23 22:20:50 +0545

    Added cart number, footer links

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { toast } from 'react-toastify';
 import axios from 'axios';
 
 import UserContext from './context/UserContext';
+import CartContext from './context/CartContext';
 
 // Components
 import Home from './components/pages/Home/Home';
@@ -62,6 +63,8 @@ function App() {
         user: undefined,
     });
 
+    const [cartData, setCartData] = useState(0);
+
     useEffect(() => {
         const checkLoggedIn = async () => {
             let token = localStorage.getItem('auth-token');
@@ -88,6 +91,13 @@ function App() {
                 });
             }
         };
+
+        const cart = async () => {
+            let cartNumber = JSON.parse(localStorage.getItem('cart')).length;
+            setCartData(cartNumber);
+        };
+        
+        cart();
         checkLoggedIn();
     }, []);
 
@@ -112,212 +122,234 @@ function App() {
     return (
         <BrowserRouter>
             <UserContext.Provider value={{ userData, setUserData }}>
-                <div className="App">
-                    <Route
-                        exact
-                        path={[
-                            '/',
-                            '/login',
-                            '/signup',
-                            '/forgot-password',
-                            '/reset-password/:token',
-                            '/verify-email/:token',
-                            '/items/:itemId',
-                            '/renter/items/add',
-                            '/renter/items',
-                            '/cart',
-                            '/orders',
-                            '/checkout',
-                            '/renter/items/edit/:itemId',
-                            '/checkout',
-                            '/checkout/complete',
-                            '/renter/order',
-                            '/category/:categoryName',
-                            '/about-us',
-                            '/wishlist',
-                            '/edit-profile',
-                            '/profile',
-                            '/change-password',
-                            '/s',
-                        ]}
-                        component={Header}
-                    />
+                <CartContext.Provider value={{ cartData, setCartData }}>
+                    <div className="App">
+                        <Route
+                            exact
+                            path={[
+                                '/',
+                                '/login',
+                                '/signup',
+                                '/forgot-password',
+                                '/reset-password/:token',
+                                '/verify-email/:token',
+                                '/items/:itemId',
+                                '/renter/items/add',
+                                '/renter/items',
+                                '/cart',
+                                '/orders',
+                                '/checkout',
+                                '/renter/items/edit/:itemId',
+                                '/checkout',
+                                '/checkout/complete',
+                                '/renter/order',
+                                '/category/:categoryName',
+                                '/about-us',
+                                '/wishlist',
+                                '/edit-profile',
+                                '/profile',
+                                '/change-password',
+                                '/s',
+                            ]}
+                            component={Header}
+                        />
 
-                    <Route exact path="/" component={Home} />
+                        <Route exact path="/" component={Home} />
 
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/signup" component={Signup} />
-                    <Route
-                        exact
-                        path="/forgot-password"
-                        component={ForgotPassword}
-                    />
-                    <Route
-                        exact
-                        path="/reset-password/:token"
-                        component={ResetPassword}
-                    />
-                    <Route
-                        exact
-                        path="/verify-email/:token"
-                        component={VerifyEmail}
-                    />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/signup" component={Signup} />
+                        <Route
+                            exact
+                            path="/forgot-password"
+                            component={ForgotPassword}
+                        />
+                        <Route
+                            exact
+                            path="/reset-password/:token"
+                            component={ResetPassword}
+                        />
+                        <Route
+                            exact
+                            path="/verify-email/:token"
+                            component={VerifyEmail}
+                        />
 
-                    <Route exact path="/items/:itemId" component={ItemDetail} />
+                        <Route
+                            exact
+                            path="/items/:itemId"
+                            component={ItemDetail}
+                        />
 
-                    <Route exact path="/renter/items/add" component={AddItem} />
+                        <Route
+                            exact
+                            path="/renter/items/add"
+                            component={AddItem}
+                        />
 
-                    <Route exact path="/renter/items" component={ViewItem} />
+                        <Route
+                            exact
+                            path="/renter/items"
+                            component={ViewItem}
+                        />
 
-                    <Route exact path="/cart" component={Cart} />
-                    <Route exact path="/orders" component={Order} />
+                        <Route exact path="/cart" component={Cart} />
+                        <Route exact path="/orders" component={Order} />
 
-                    <Route exact path="/checkout" component={Checkout} />
-                    <Route
-                        exact
-                        path="/checkout/complete"
-                        component={CheckoutComplete}
-                    />
+                        <Route exact path="/checkout" component={Checkout} />
+                        <Route
+                            exact
+                            path="/checkout/complete"
+                            component={CheckoutComplete}
+                        />
 
-                    <Route
-                        exact
-                        path="/renter/items/edit/:itemId"
-                        component={EditItem}
-                    />
+                        <Route
+                            exact
+                            path="/renter/items/edit/:itemId"
+                            component={EditItem}
+                        />
 
-                    <Route exact path="/renter/order" component={RentOrder} />
-                    <Route exact path="/about-us" component={About} />
-                    <Route exact path="/wishlist" component={Wishlist} />
-                    <Route
-                        exact
-                        path="/category/:categoryName"
-                        component={Category}
-                    />
+                        <Route
+                            exact
+                            path="/renter/order"
+                            component={RentOrder}
+                        />
+                        <Route exact path="/about-us" component={About} />
+                        <Route exact path="/wishlist" component={Wishlist} />
+                        <Route
+                            exact
+                            path="/category/:categoryName"
+                            component={Category}
+                        />
 
-                    <Route
-                        exact
-                        path="/change-password"
-                        component={ChangePassword}
-                    />
-                    <Route exact path="/edit-profile" component={EditProfile} />
+                        <Route
+                            exact
+                            path="/change-password"
+                            component={ChangePassword}
+                        />
+                        <Route
+                            exact
+                            path="/edit-profile"
+                            component={EditProfile}
+                        />
 
-                    <Route exact path="/profile" component={Profile} />
+                        <Route exact path="/profile" component={Profile} />
 
-                    <Route exact path="/s" component={SearchPage} />
+                        <Route exact path="/s" component={SearchPage} />
 
-                    {/* Admin */}
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin"
-                        component={Admin}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/users"
-                        component={UserAdmin}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/users/add"
-                        component={AddUser}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/users/edit/:userId"
-                        component={EditUser}
-                        isAdmin={isAdmin()}
-                    />
+                        {/* Admin */}
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin"
+                            component={Admin}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/users"
+                            component={UserAdmin}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/users/add"
+                            component={AddUser}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/users/edit/:userId"
+                            component={EditUser}
+                            isAdmin={isAdmin()}
+                        />
 
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/item"
-                        component={ItemAdmin}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/items/add"
-                        component={AddItemAdmin}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/items/edit/:itemId"
-                        component={EditItemAdmin}
-                        isAdmin={isAdmin()}
-                    />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/item"
+                            component={ItemAdmin}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/items/add"
+                            component={AddItemAdmin}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/items/edit/:itemId"
+                            component={EditItemAdmin}
+                            isAdmin={isAdmin()}
+                        />
 
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/categories"
-                        component={AdminCategory}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/categories/add"
-                        component={AddCategoryAdmin}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/categories/edit/:categoryId"
-                        component={EditCategoryAdmin}
-                        isAdmin={isAdmin()}
-                    />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/categories"
+                            component={AdminCategory}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/categories/add"
+                            component={AddCategoryAdmin}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/categories/edit/:categoryId"
+                            component={EditCategoryAdmin}
+                            isAdmin={isAdmin()}
+                        />
 
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/sub-categories"
-                        component={SubCategory}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/sub-categories/add"
-                        component={AddSubCategory}
-                        isAdmin={isAdmin()}
-                    />
-                    <ProtectedAdminRoute
-                        exact
-                        path="/admin/sub-categories/edit/:subCategoryId"
-                        component={EditSubCategory}
-                        isAdmin={isAdmin()}
-                    />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/sub-categories"
+                            component={SubCategory}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/sub-categories/add"
+                            component={AddSubCategory}
+                            isAdmin={isAdmin()}
+                        />
+                        <ProtectedAdminRoute
+                            exact
+                            path="/admin/sub-categories/edit/:subCategoryId"
+                            component={EditSubCategory}
+                            isAdmin={isAdmin()}
+                        />
 
-                    <Route
-                        exact
-                        path={[
-                            '/',
-                            '/login',
-                            '/signup',
-                            '/forgot-password',
-                            '/reset-password/:token',
-                            '/verify-email/:token',
-                            '/items/:itemId',
-                            '/renter/items/add',
-                            '/renter/items',
-                            '/cart',
-                            '/orders',
-                            '/checkout',
-                            '/renter/items/edit/:itemId',
-                            '/checkout',
-                            '/checkout/complete',
-                            '/renter/order',
-                            '/category/:categoryName',
-                            '/about-us',
-                            '/wishlist',
-                            '/edit-profile',
-                            '/profile',
-                            '/change-password',
-                            '/s',
-                        ]}
-                        component={Footer}
-                    />
-                </div>
+                        <Route
+                            exact
+                            path={[
+                                '/',
+                                '/login',
+                                '/signup',
+                                '/forgot-password',
+                                '/reset-password/:token',
+                                '/verify-email/:token',
+                                '/items/:itemId',
+                                '/renter/items/add',
+                                '/renter/items',
+                                '/cart',
+                                '/orders',
+                                '/checkout',
+                                '/renter/items/edit/:itemId',
+                                '/checkout',
+                                '/checkout/complete',
+                                '/renter/order',
+                                '/category/:categoryName',
+                                '/about-us',
+                                '/wishlist',
+                                '/edit-profile',
+                                '/profile',
+                                '/change-password',
+                                '/s',
+                            ]}
+                            component={Footer}
+                        />
+                    </div>
+                </CartContext.Provider>
             </UserContext.Provider>
         </BrowserRouter>
     );
